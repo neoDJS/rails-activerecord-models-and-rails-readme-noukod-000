7af3a57c50a68fc5072c69af2a011e1213bfbe3e
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-01-29 20:09:41 +0000

    Automatically backed up by Learn

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 describe Post do
-
-end
+    it 'can be created' do
+    post = Post.create!(title: "My title", description: "The post description")
+    expect(post).to be_valid
+  endend
